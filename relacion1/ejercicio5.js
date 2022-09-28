d35c46712218ad8b295319391f1ca5f449c71492
angel2 <angelote2229914@gmail.com>
2022-09-28 12:56:46 +0200

    commit de locos

--- relacion1/ejercicio5.js
+++ relacion1/ejercicio5.js
@@ -1,5 +1,5 @@
 function mostrarNumeros(){
     for (let index = 1; index <= 15; index++) {
-        document.write(`<h${index}> ${index} </h${index}>`);
+        document.write(`<h${index}> ${index} titulo </h${index}>`);
     }
 }
